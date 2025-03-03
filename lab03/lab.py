385fa7da5ad70e846eb36540a194ee1ae93635c8
alimkanova-t-auca-2022 <alimkanova_t@auca.kg>
2025-03-03 21:50:10 +0600

    finish lab03

---
+++ lab03/lab.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import matplotlib.pyplot as pyplot
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.preprocessing import LabelEncoder
+
+data_frame = pd.read_csv("lab03\PL_Players.csv", encoding='latin-1')
+data_frame.dropna(inplace=True)
+data_frame = data_frame.rename(columns={"Pos" : "Position",
+                           "90s" : "Total minutes played divided by 90",
+                           "Tkl" : "Tackles",
+                           "TklW" : "Tackles Won",
+                           "Def 3r" : "Tackles won in defensive 3rd",
+                           "Mid 3r" : "Tackles won in midfield",
+                           "Att 3r" : "Tackles won in attacking 3rd",
+                           "Press" : "number of times opponent put under pressure",
+                           "Succ" : "Successful press",
+                           "%" : "% of successful press",
+                           "Def 3rd" : "Press in defense",
+                           "Mid 3rd" : "Press in midfield",
+                           "Att 3rd" : "Press in attack",
+                           "Blocks" : "Blocked the ball",
+                           "Pass" : "Passes blocked",
+                           "Int" : "Interceptions",
+                           "Tkl+Int" :"Tackles+Interceptions",
+                           "Clr" : "Clearances",
+                           "Err" : "Errors leading to goal"})
+
+x = data_frame[["Tackles", "Tackles Won", "Tackles+Interceptions", "Interceptions", "Clearances", "Successful press", "Press in defense", "Passes blocked"]]
+#x = data_frame[["Blocked the ball", "Passes blocked"]]
+y = data_frame["Position"] 
+
+encoder = LabelEncoder()
+y = encoder.fit_transform(y)
+
+x_train, x_test, y_train, y_test = train_test_split(x,y, test_size =0.2, stratify=y, random_state =42)
+
+n = int(input("Enter n: "))
+k_nearest_n = KNeighborsClassifier(n_neighbors = n)
+k_nearest_n.fit(x_train, y_train)
+prediction = k_nearest_n.predict(x_test)
+
+print("Accuracy:", accuracy_score(y_test, prediction))
+print("Classification Report:\n", classification_report(y_test,prediction, zero_division=1))
